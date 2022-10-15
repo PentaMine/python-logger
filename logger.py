66aa6d9bd88aaafc7b46d8449e84a6358400f838
PentaMine <83116574+PentaMine@users.noreply.github.com>
2022-10-15 19:35:52 +0200

    changed program structure to a more obj-o one

--- logger.py
+++ logger.py
@@ -1,65 +1,62 @@
 from datetime import datetime
 from colorama import Fore
 
-logLevel = True
-colorLevelText = True
-quitWhenLogFatal = True
-colorText = False
 
-warnColor = Fore.YELLOW
-infoColor = Fore.RESET
-debugColor = Fore.WHITE
-errorColor = Fore.LIGHTRED_EX
-fatalColor = Fore.RED
+class Logger:
+    class LogLevel:
+        def __init__(self, name, color: str = Fore.RESET, isFatal: bool = False):
+            self.name = name
+            self.color = color
+            self.isFatal = isFatal
 
+        def log(self, message):
+            logger = Logger()
+            logger.log(message, self)
+            if self.isFatal and Logger.quitWhenLogFatal:
+                quit()
 
-class CustomLogLevel:
-    def __init__(self, name, color: str = Fore.RESET, isFatal: bool = False):
-        self.name = name
-        self.color = color
-        self.isFatal = isFatal
+    logLevel = True
+    colorLevelText = True
+    quitWhenLogFatal = True
+    colorText = True
+    warnLevel = LogLevel("WARN", Fore.YELLOW)
+    infoLevel = LogLevel("INFO", Fore.RESET)
+    debugLevel = LogLevel("DEBUG", Fore.WHITE)
+    errorLevel = LogLevel("ERROR", Fore.LIGHTRED_EX)
+    fatalLevel = LogLevel("FATAL", Fore.RED)
 
-    def log(self, message):
-        log(message, self.color, self.name)
-        if self.isFatal and quitWhenLogFatal:
-            quit()
+    # def log(self, message: str, color: str = Fore.RESET, level: LogLevel = "DEBUG", end: str = "\n"):
+    def log(self, message: str, level: LogLevel = debugLevel, end: str = "\n"):
+        current_time = datetime.now()
+        if Logger.colorText:
+            if Logger.logLevel:
+                print(
+                    f"[{current_time.strftime('%X')}] {level.color if Logger.colorLevelText else Fore.RESET}[{level.name}]",
+                    level.color + str(message), end=end + Fore.RESET)
 
-
-def log(message: str, color: str = Fore.RESET, level: str = "DEBUG", end: str = "\n"):
-    current_time = datetime.now()
-    if colorText:
-        if logLevel:
-            print(f"[{current_time.strftime('%X')}] {color if colorLevelText else Fore.RESET}[{level}]",
-                  color + str(message), end=end + Fore.RESET)
-
+            else:
+                print(f"[{current_time.strftime('%X')}]", level.color + str(message), end=end + Fore.RESET)
         else:
-            print(f"[{current_time.strftime('%X')}]", color + str(message), end=end + Fore.RESET)
-    else:
-        if logLevel:
-            print(f"[{current_time.strftime('%X')}] [{level}]",
-                  str(message), end=end)
+            if Logger.logLevel:
+                print(f"[{current_time.strftime('%X')}] [{level.name}]",
+                      str(message), end=end)
 
-        else:
-            print(f"[{current_time.strftime('%X')}]", str(message), end=end)
+            else:
+                print(f"[{current_time.strftime('%X')}]", str(message), end=end)
 
+    def log_warning(self, message: str):
+        self.log(message, Logger.warnLevel)
 
-def log_warning(message: str):
-    log(message, warnColor, "WARN")
+    def log_info(self, message: str):
+        self.log(message, Logger.infoLevel)
 
+    def log_debug(self, message: str):
+        self.log(message, Logger.debugLevel)
 
-def log_info(message: str):
-    log(message, infoColor, "INFO")
+    def log_error(self, message: str):
+        self.log(message, Logger.errorLevel)
 
-
-def log_debug(message: str):
-    log(message, debugColor, "DEBUG")
-
-
-def log_error(message: str):
-    log(message, errorColor, "ERROR")
-
-
-def log_fatal(message: str, exception=""):
-    log(message, fatalColor, "FATAL")
-    if quitWhenLogFatal:
-        quit(exception)
+    def log_fatal(self, message: str, exception=""):
+        self.log(message, Logger.fatalLevel)
+        if Logger.quitWhenLogFatal:
+            quit(exception)
